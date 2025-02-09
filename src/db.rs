d38090773d9a6746832c576ce0c155c42a9c6367
og.rondon <og.rondon@gmail.com>
2025-02-09 18:18:09 -0400

    add env variables

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,24 @@
-use tokio_postgres::{NoTls, Client, Error};
+use dotenv::dotenv;
+use std::env;
+use tokio_postgres::{Client, Error, NoTls};
 
 pub struct DbClient(pub Client);
 
 pub async fn conection_db() -> Result<Client, Error> {
+    dotenv().ok();
+
     let (client, connection) = tokio_postgres::connect(
-        "host=localhost user=postgres password=123 dbname=finanzas_QA",
-        NoTls
-    ).await?;
+        format!(
+            "host=${} user=${} password=${} dbname=${}",
+            env::var("HOST").expect("err"),
+            env::var("USER").expect("err"),
+            env::var("PWD").expect("err"),
+            env::var("DATABASE").expect("err"),
+        )
+        .as_str(),
+        NoTls,
+    )
+    .await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
